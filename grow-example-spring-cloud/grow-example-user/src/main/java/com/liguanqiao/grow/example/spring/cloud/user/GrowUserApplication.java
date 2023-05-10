f4f1a31b08934b26fa3c4c4a7274f53ce1866aed
liguanqiao <996829113@qq.com>
2023-05-10 20:38:42 +0800

    暂存代码

---
+++ grow-example-spring-cloud/grow-example-user/src/main/java/com/liguanqiao/grow/example/spring/cloud/user/GrowUserApplication.java
@@ -0,0 +1,19 @@
+package com.liguanqiao.grow.example.spring.cloud.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author liguanqiao
+ * @since 2023/5/5
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GrowUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowOrdersApplication.class);
+    }
+
+}
